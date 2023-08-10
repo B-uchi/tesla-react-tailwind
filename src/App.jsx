28baf29452e97ac6f18a69281bedc7b2c912f187
Ibuchukwu Umeano <ibumeano@gmail.com>
2023-08-10 12:20:28 +0100

    tailwind set up

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <div className="bg-slate-500 w-2">
+        <h1>Hello World</h1>
+        <p>This is a test paragraph element</p>
+      </div>
+    </>
+  );
+};
+
+export default App;
